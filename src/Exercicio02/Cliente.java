688760bf38490e6facc227963eddabdd4a7b0cc6
Samuel <samuca12494@gmail.com>
2026-04-02 11:00:00 -0300

    Exercicio2 feito

---
+++ src/Exercicio02/Cliente.java
@@ -0,0 +1,36 @@
+package Exercicio02;
+
+
+public class Cliente {
+
+    private String nome;
+    private  int senha;
+
+
+    public Cliente(String nome, int senha) {
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+
+    @Override
+    public String toString() {
+        return nome + " - Senha: " + senha;
+    }
+}
---
+++ src/Exercicio02/Exercicio02.java
@@ -0,0 +1,25 @@
+package Exercicio02;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+
+        Fila<Cliente> fila = new Fila<Cliente>(6);
+
+        fila.enfileira(new Cliente("Samuel", 1));
+        fila.enfileira(new Cliente("Icaro", 2));
+        fila.enfileira(new Cliente("Keren", 3));
+        fila.enfileira(new Cliente("Marco", 4));
+        fila.enfileira(new Cliente("Pedro", 5));
+        fila.enfileira(new Cliente("Julia", 6));
+
+        System.out.println(fila);
+
+        while(!fila.estaVazia()){
+            Cliente c = fila.desenfileira();
+            System.out.println("Chamando: " + c);
+        }
+
+        System.out.println("\nFila vazia.");
+
+    }
+}
